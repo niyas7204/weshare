1a18208d1a510ba60e216998c74d95df81a84345
niyas7204 <mohammadniyas7204@gmail.com>
2023-10-24 10:15:42 +0530

    update graphql

--- lib/data/data_handler/follow_implimentation.dart
+++ lib/data/data_handler/follow_implimentation.dart
@@ -1,50 +1,35 @@
 import 'dart:developer';
 
 import 'package:graphql/client.dart';
-import 'package:weshare/client/get.dart';
-import 'package:weshare/client/graphql_client.dart';
+import 'package:weshare/graphql/client/graphql_client.dart';
 import 'package:weshare/core/helpers/api_response_handler.dart';
 import 'package:weshare/data/repository/user_profileservice.dart';
+import 'package:weshare/graphql/query/follow_and_unfollow.dart';
+import 'package:weshare/graphql/query/get_all_data.dart';
 
 class FollowImplimentation implements FollowService {
+  //follow an account
   @override
   Future<StateResponse> followAccount(
-      {required String accoutId, required String userId}) async {
-    final String mutation = '''mutation insertFollower {
-  insert_friends_one(object: {follower: "$userId", head: "$accoutId"}) {
-    user {
-      friends(where: {userByHead: {userId: {_eq: "$accoutId"}}}) {
-        userByHead {
-          email
-          profileImage
-          userId
-          user_Name
-          postsBySenderid {
-            textFeed
-            tags
-            senderName
-            senderId
-            postId
-            likes
-            imageFeed
-          }
-        }
-      }
-    }
-  }
-}
-''';
+      {required String accountId, required String userId}) async {
+    final followMutation =
+        FollowAndUnfollowMutation(userId: userId, accountId: accountId);
 
     try {
       final getQuery = GetUserQuery(userId: userId);
       final mutationOption = MutationOptions(
-        document: gql(mutation),
+        document: gql(followMutation.followMutation),
         update: (cache, result) {
           final data = cache
               .readQuery(QueryOptions(document: gql(getQuery.query)).asRequest);
           List friends = data!['user'][0]["friends"];
           friends
               .addAll(result!.data!["insert_friends_one"]["user"]["friends"]);
+          final updatedData = Map<String, dynamic>.from(data);
+          updatedData["user"][0]["friends"] = friends;
+          cache.writeQuery(
+              QueryOptions(document: gql(getQuery.query)).asRequest,
+              data: updatedData);
         },
       );
 
@@ -58,29 +43,31 @@ class FollowImplimentation implements FollowService {
     }
   }
 
+//unfollow an account
   @override
   Future<StateResponse> unfollowAccount(
-      {required String accoutId, required String userId}) async {
-    final String mutation = '''mutation MyMutation {
-  delete_friends(where: {follower: {_eq: "$userId"}, head: {_eq: "$accoutId"}}) {
-    returning {
-      head
-    }
-  }
-}''';
-
+      {required String accountId, required String userId}) async {
     try {
       final getQuery = GetUserQuery(userId: userId);
+      final followMutation =
+          FollowAndUnfollowMutation(userId: userId, accountId: accountId);
 
       final mutationOption = MutationOptions(
-        document: gql(mutation),
+        document: gql(followMutation.unfollowMutation),
         update: (cache, result) {
           final id = result!.data!["delete_friends"]["returning"][0]["head"];
+          log('result $result');
           final data = cache
               .readQuery(QueryOptions(document: gql(getQuery.query)).asRequest);
-          final friends = data!['user'][0]["friends"];
+          log('cache data $data');
+          final friends = data!["user"][0]["friends"];
           friends
-              .removeWhere((element) => element["userByHEad"]["userId"] == id);
+              .removeWhere((element) => element["userByHead"]["userId"] == id);
+          final updatedData = Map<String, dynamic>.from(data);
+          updatedData["user"][0]["friends"] = friends;
+          cache.writeQuery(
+              QueryOptions(document: gql(getQuery.query)).asRequest,
+              data: updatedData);
         },
         onCompleted: (data) {},
       );
